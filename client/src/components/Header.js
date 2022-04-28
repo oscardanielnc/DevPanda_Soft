8bb6ce02138b93e7809e21f878a8554ded53fe31
Jeison1712 <jeison.romero@pucp.edu.pe>
2022-04-27 20:32:21 -0500

    Mi primer commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,25 +7,25 @@ import './Header.scss';
 export default function Header (props) {
     return (
         <div className="header">
-                <div className="header__titulo">
-                    <img src={Logo} alt="Jeison Romero"
-                    style={{marginTop: "10px"}}
-                    />
-                </div>
-                <div className="header__menu">
-                    <Link className="header__menu-option" to="/" >Home</Link>
-                    <Link className="header__menu-option" to="/home2" >Home2</Link>
-                </div>
-                <div className="header__nombreUsuario">
-                    <div>
-                        Coordinador:<br/>
-                    </div>
-                    <div>
-                        Carlos Alberto<br/>
+                <div className="header__left">
+                    <div className="header__lefttitulo">
+                        <img src={Logo} alt="Jeison Romero"
+                        style={{marginTop: "10px"}}
+                        />
                     </div>
                 </div>
-                <div className="header__usuarioImagen">
-                    <img src={LogoUsuario} alt="Jeison Romero"/>
+                <div className="header__right">
+                    <div className="header__rightnombreUsuario">
+                        <div>
+                            Coordinador:<br/>
+                        </div>
+                        <div>
+                            Carlos Alberto<br/>
+                        </div>
+                    </div>
+                    <div className="header__rightusuarioImagen">
+                        <img src={LogoUsuario} alt="Jeison Romero"/>
+                    </div>
                 </div>
             </div>
     )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import LayoutBasic from "../layouts/LayoutBasic";
 import {sayHelloApi} from '../api/example'
 import ModalExample from "../components/Modals/ModalExample";
+import './Home.scss';
 
 export default function Home () {
     const [data, setData] = useState(null);
@@ -15,14 +16,68 @@ export default function Home () {
         // con la siguiente linea hacemos visible el Modal
         setShowModal(true)
     }
+    const agregarEspecialidad=()=>{
 
+    }
+
+    const dataEspecialidades=[
+        {
+            "idEspecialidad": 1,
+            "nombreEsp": "Ingeniería Informática",
+            "flagMatricula": 1,
+            "flagConvenio": 0
+        },
+        {
+            "idEspecialidad": 2,
+            "nombreEsp": "Ingeniería Industrial",
+            "flagMatricula": 1,
+            "flagConvenio": 0
+        },
+        {
+            "idEspecialidad": 3,
+            "nombreEsp": "Ingeniería de Telecomunicaciones",
+            "flagMatricula": 1,
+            "flagConvenio": 0
+        },
+        {
+            "idEspecialidad": 4,
+            "nombreEsp": "Ingeniería Electrónica",
+            "flagMatricula": 1,
+            "flagConvenio": 0
+        }
+    ]
+    
     return (
         <LayoutBasic>
+            <div style={{textAlign: "center"}}>
+                <h1>Gestión de especialidades</h1>
+            </div>
+            <div style={{textAlign: "right"}}>
+            <button type="button" class="btn btn-primary" onClick={agregarEspecialidad} style={{"marginRight":"50px"}} >Agregar</button>
+            </div>
             <div>
-                <h1>We're in Home</h1>
-                <button onClick={sayHello}>Tócame!</button>
+            <button onClick={sayHello}>Tócame!</button>
                 {data && <h3>{`Mensaje: ${data}`}</h3>}
             </div>
+            <div class="container" style={{marginBottom:"20px"}}>
+                <div class="row">
+                <div class="col-sm-8" style={{"marginTop":"15px",textAlign: "center"}}>Nombre Especialidad</div>                    
+                <div class="col-sm-4" style={{"marginTop":"15px",textAlign: "left"}}>Acciones</div>
+                </div>
+            </div>
+            <div class="container">
+            {
+                dataEspecialidades.map((element,index)=>(
+                    <div class="row" key={index}>
+                    <div class="col-sm-8" style={{"marginTop":"15px"}}>{element.nombreEsp}</div>                    
+                    <div class="col-sm-2" style={{"marginTop":"15px"}}><button type="button" class="btn btn-primary">Editar</button></div>
+                    <div class="col-sm-2" style={{"marginTop":"15px"}}><button type="button" class="btn btn-danger">Eliminar</button></div>
+                    </div>
+                ))
+
+            }
+            </div>
+                
             
             <ModalExample show={showModal}
                 setShow={setShowModal}
