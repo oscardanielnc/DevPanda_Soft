cf7440ba2d0a2b056ab20ddab1b736a320d334de
Call2608 <a20161313@pucp.edu.pe>
2022-05-02 10:28:12 -0500

    Carlos Avance Lunes

--- client/src/layouts/LayoutBasic.js
+++ client/src/layouts/LayoutBasic.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import StudentNavBar from "../components/navBar/StudentNavbar"
 
 
 import './LayoutBasic.scss';
@@ -9,10 +10,21 @@ export default function LayoutBasic (props) {
     const {children} = props;
 
     return (
-        <div className="layout-basic">
-            <Header />
-            {children}
-            <Footer />
+        <div class="container"> 
+            <div className="layout-basic">
+                <Header />
+                
+                    <div class = "row">
+                        <div class = "col-sm-3">
+                            <StudentNavBar/>
+                        </div>
+                        <div className = "col align-self-center">
+                            {children}
+                        </div>
+                    </div>    
+                        
+                <Footer />
+            </div>
         </div>
     )
 }
