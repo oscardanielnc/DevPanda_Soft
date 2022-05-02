08aaabfed9728d845e4a102a464987a6f8f00106
Josue Echandia <jechandi@emeal.nttdata.com>
2022-05-01 23:42:36 -0500

    se completo StateViewer

--- client/src/components/StateViewer/StateViewer.js
+++ client/src/components/StateViewer/StateViewer.js
@@ -1,8 +1,32 @@
-import React, {useState} from "react";
-import { Button, Table,Form,InputGroup,FormControl } from 'react-bootstrap';
+import React  from "react";
 
 import './StateViewer.scss';
 
+const SuccessImage = "./StateViewerImages/success.png"
+const FileEmptyImage = "./StateViewerImages/fileEmpty.png"
+const PendingImage = "./StateViewerImages/pending.png"
+const WarningImage = "./StateViewerImages/warning.png"
+
+export class StatesViewType {
+    constructor (title, image, description) {
+    this.title = title;
+    this.image = image;
+    this.description = description;
+    }
+
+    static success( title, description){
+        return  new StatesViewType(title, SuccessImage, description)
+    }
+    static fileEmpty( title, description){
+        return  new StatesViewType(title, FileEmptyImage, description)
+    }
+    static pending( title, description){
+        return  new StatesViewType(title, PendingImage, description)
+    }
+    static warning( title, description){
+        return  new StatesViewType(title, WarningImage, description)
+    }
+}
 
 export default function StateViewer ({states}) {
     return (
