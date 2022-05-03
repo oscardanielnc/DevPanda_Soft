8a4e04c6601e240586af31d33128992de904554b
FaridFranz <farid@MSI>
2022-05-02 22:23:50 -0500

    Mi primer commit

--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -2,6 +2,8 @@ import GestionEspecialidad from "../pages/GestionEspecialidad";
 import AgregarEspecialidad from "../pages/AgregarEspecialidad";
 import StudentRegistrationForm from "../pages/StudentRegistrationForm";
 import Error404 from "../pages/Error404";
+import FACINavbar from "../components/navBarCoordFACI/FACINavbar";
+import StudentNavbar from "../components/navBar/StudentNavbar";
 
 const routes = [
     {
@@ -18,6 +20,14 @@ const routes = [
         path: "/studentRegistrationForm",
         exact: true,
         component: StudentRegistrationForm,
+    },
+    {
+        path: "/FACInavbar",
+        component: FACINavbar,
+    },
+    {
+        path: "/Stunavbar",
+        component: StudentNavbar,
     },
     {
         path: "*",
