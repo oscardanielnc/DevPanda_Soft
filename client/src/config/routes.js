fa3e0b02471e4c287b8ce39c04b8c32b90b1dbaa
JavierPaP <a20162756@pucp.edu.pe>
2022-05-02 22:41:10 -0500

    Mi primer push

--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -1,6 +1,10 @@
 import GestionEspecialidad from "../pages/GestionEspecialidad";
 import AgregarEspecialidad from "../pages/AgregarEspecialidad";
 import StudentRegistrationForm from "../pages/StudentRegistrationForm";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+import RevConveniosCoordEsp from "../pages/RevisionConveniosCoordEsp";
+
 import Error404 from "../pages/Error404";
 
 const routes = [
@@ -18,6 +22,21 @@ const routes = [
         path: "/studentRegistrationForm",
         exact: true,
         component: StudentRegistrationForm,
+    },
+    {
+        path: "/signin",
+        exact: true,
+        component: SignIn,
+    },
+    {
+        path: "/signup",
+        exact: true,
+        component: SignUp,
+    },
+    {
+        path: "/revisionconvenioscoordinadoresp",
+        exact: true,
+        component: RevConveniosCoordEsp,
     },
     {
         path: "*",
