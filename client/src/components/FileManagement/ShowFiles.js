f1760838383b4cef7329c80b27476e2e0e42a23e
Marcelo <marcelox1609@gmail.com>
2022-05-11 00:28:58 -0500

    AgreementReview casi terminado + fixes de FileManagement

--- client/src/components/FileManagement/ShowFiles.js
+++ client/src/components/FileManagement/ShowFiles.js
@@ -3,7 +3,7 @@ import DocumentPlusIcon from "../DocumentPlusIcon/DocumentPlusIcon";
 
 export default function ShowFiles({docs}) {
     return (
-        <div className="row rows">
+        <div className="row normalrow" style={{marginTop:"5px",marginBottom:"10px"}}>
             {
                 docs.length>0 && docs.map((e, index) => (
                     <DocumentPlusIcon name={e.nombre} path={e.ruta} key={index}/>
--- client/src/pages/AgreementReview.js
+++ client/src/pages/AgreementReview.js
@@ -1,4 +1,4 @@
-import React,{ useState } from "react";
+import React,{ useState,useEffect } from "react";
 import LayoutCoordFACI from "../layouts/LayoutCoordFACI";
 import StateViewer,{StatesViewType} from "../components/StateViewer/StateViewer";
 import DocumentPlusIcon from "../components/DocumentPlusIcon/DocumentPlusIcon";
@@ -6,106 +6,214 @@ import { Button, Form} from "react-bootstrap";
 import Convenio from "../asserts/img/pdf/Convenio.pdf"
 import "./AgreementReview.scss";
 import FileManagement from "../components/FileManagement/FileManagement";
+import ShowFiles from "../components/FileManagement/ShowFiles";
+import { getAllDocsApi } from "../api/files";
 
-// "fileData" : {           
-//     //TO DO
-// },
+//TO DO
+//1) Realizar el select
+//2) Realizar los updates de faci y de especialidad
 
-// "fileReview" :{
-//     //TO DO
-// },
-
+const idAlumno = 1;
+//const idCoordinador ="";
+const tipoPersonal = "F"; //F = FACI, E = Especialidad // NO
+//const idEntregaConvenio= 1; // NO
 const dataDummy = {
-    "faciState" : "Aprobado", // consumir API GET "Aprobado,Pendiente de revision, Observado"
-    "espState" : "Pendiente de revision",// consumir API GET "Aprobado,Pendiente de revision, Observado"
-    "reviewCoord" :{
-        "documentState" : "Observado", //"Aprobado,Pendiente de revision, Observado"
-       "observations" : "JIANFRANCO",           
+    "entregaConvenioyPlan":{        
+        "faciState" : "Aprobado", // consumir API GET (A = "Aprobado", P = "Pendiente" de revisión, O ="Observado")
+        "espState" : "Observado",    // consumir API GET  (A = Aprobado, P = Pendiente de revisión, O = Observado)
+        "observations" : "Bien hecho crack"        
     }
 }
+export default function AgreementReview (){
 
-export default function AgreementReview (){      
-        
+    const [data, setData] = useState(dataDummy); 
+    const [docs, setDocs] = useState([])
+    const [docsStudent, setDocsStudent] = useState([])
+    const [docsCoord, setDocsCoord] = useState([])
 
-    const [data, setData] = useState(dataDummy);
+    useEffect(() => {
+        getAllDocsApi("1-1-CONV", 0).then(response => {
+            if(response.success) {
+                setDocs(response.docs)
+            }
+        })
+    },[setDocs])
 
-    const  save = e => {
-		e.preventDefault();//Permite ya no recargar el formulario
-		//console.log(data);
-	};
+    useEffect(() => {
+        getAllDocsApi(`1-1-CONV-${idAlumno}`, 1).then(response => {
+            if(response.success) {
+                setDocsStudent(response.docs)
+            }
+        })
+    },[setDocsStudent])
 
-    /* --- */
-    let pass=(data.reviewCoord.documentState==="Aprobado")?true:false;
-    let pending=(data.reviewCoord.documentState==="Pendiente de revision")?true:false;
-    let observed=(data.reviewCoord.documentState==="Observado")?true:false;
+    useEffect(() => {
+        getAllDocsApi(`1-1-CONV-${idAlumno}`, 0).then(response => {
+            if(response.success) {
+                setDocsCoord(response.docs)
+            }
+        })
+    },[setDocsCoord])
+
+    let documentState ="";
+    if(tipoPersonal === "F")        
+        documentState = data.entregaConvenioyPlan.faciState;
+    else
+        if(tipoPersonal === "E")
+            documentState = data.entregaConvenioyPlan.espState;
+    
+    // useEffect(()=> {
+    //     /*
+    //     selectAgreementReview(data.idCoordinador, idEntregaConvenio).then(response => {
+    //         if(response.success) {
+    //             setData(response.data);
+    //         }
+    //     })*/
+    // }, [setData])
+    
+    let result=true;
+    const update = async e => {
+        //se hace una diferencia entre si es coordinador FACI o de Especialidad        
+        /*
+        e.preventDefault();
+        if(numDocumentos === 2){
+            let response=null;
+            if(tipoPersonal==="F"){               
+                response = await agreementAndPlanReviewFACIUpdate(data);
+            }
+            if(tipoPersonal==="E"){
+                response = await agreementAndPlanReviewEspUpdate(data);
+            }
+        }        
+        */        
+    }
+    
+    let pass=(documentState==="Aprobado")?true:false;
+    let pending=(documentState==="Pendiente")?true:false;
+    let observed=(documentState==="Observado")?true:false;    
+    let comentarioFACI="";
+    let comentarioEsp="";
+    
+    if(data.entregaConvenioyPlan.espState === "Aprobado")
+        comentarioEsp="Aprobado";
+    else{
+        if(data.entregaConvenioyPlan.espState === "Observado")
+            comentarioEsp = "Observado";
+        else{
+            if(data.entregaConvenioyPlan.espState === "Pendiente")
+                comentarioEsp = "Pendiente de revisión";
+        }            
+    }
+
+    if(data.entregaConvenioyPlan.faciState === "Aprobado")
+        comentarioFACI="Aprobado"
+    else{
+        if(data.entregaConvenioyPlan.faciState === "Observado")
+            comentarioFACI = "Observado"
+        else{
+            if(data.entregaConvenioyPlan.faciState === "Pendiente")
+                comentarioFACI = "Pendiente de revisión"
+        }            
+    }
 
     const changeStatePassed = e => {
         pass=!pass;
-        setData({
-            ...data,            
-            reviewCoord: {
-                ...data.reviewCoord,
-                documentState : "Aprobado"
+        if(tipoPersonal === "F"){
+            setData({
+                ...data,            
+                entregaConvenioyPlan: {
+                    ...data.entregaConvenioyPlan,
+                    faciState : "Aprobado"
+                }
+            })
+        }else{
+            if(tipoPersonal === "E"){
+                setData({
+                    ...data,            
+                    entregaConvenioyPlan: {
+                        ...data.entregaConvenioyPlan,
+                        espState : "Aprobado"
+                    }
+                })
             }
-        })
+        }
     }
-
     const changeStatePending = e => {
         pending=!pending;
-        setData({
-            ...data,            
-            reviewCoord: {
-                ...data.reviewCoord,
-                documentState : "Pendiente de revision"
+        if(tipoPersonal === "F"){
+            setData({
+                ...data,            
+                entregaConvenioyPlan: {
+                    ...data.entregaConvenioyPlan,
+                    faciState : "Pendiente"
+                }
+            })
+        }else{
+            if(tipoPersonal === "E"){
+                setData({
+                    ...data,            
+                    entregaConvenioyPlan: {
+                        ...data.entregaConvenioyPlan,
+                        espState : "Pendiente"
+                    }
+                })
             }
-        })
+        }
     }
     
     const changeStateObserved = e => {
         observed=!observed;
-        setData({
-            ...data,            
-            reviewCoord: {
-                ...data.reviewCoord,
-                documentState : "Observado"
+        if(tipoPersonal === "F"){
+            setData({
+                ...data,            
+                entregaConvenioyPlan: {
+                    ...data.entregaConvenioyPlan,
+                    faciState : "Observado"
+                }
+            })
+        }else{
+            if(tipoPersonal === "E"){
+                setData({
+                    ...data,            
+                    entregaConvenioyPlan: {
+                        ...data.entregaConvenioyPlan,
+                        espState : "Observado"
+                    }
+                })
             }
-        })
+        }
     }
 
     const changeComments = e => {
         setData({
             ...data,
-            reviewCoord: {
-                ...data.reviewCoord,
+            entregaConvenioyPlan: {
+                ...data.entregaConvenioyPlan,
                 [e.target.name]: e.target.value
             }
         })
     }
-
-
-    /* --- */
-
+    
     let typeApprovalStateFACI = "";
-    switch(data.faciState) {
+    switch(data.entregaConvenioyPlan.faciState) {
         case "Observado": typeApprovalStateFACI = "warning"; break;
         case "Aprobado": typeApprovalStateFACI = "success"; break;
-        case "Pendiente de revision": typeApprovalStateFACI = "pending"; break;        
+        case "Pendiente": typeApprovalStateFACI = "pending"; break;        
         default: typeApprovalStateFACI = "error"; break;
     }
     let typeApprovalStateEsp = "";
-    switch(data.espState) {
+    switch(data.entregaConvenioyPlan.espState) {
         case "Observado": typeApprovalStateEsp = "warning"; break;
         case "Aprobado": typeApprovalStateEsp = "success"; break;
-        case "Pendiente de revision": typeApprovalStateEsp = "pending"; break;        
+        case "Pendiente": typeApprovalStateEsp = "pending"; break;        
         default: typeApprovalStateEsp = "error"; break;
     }
 
-
     return (
         <LayoutCoordFACI>
-           <div className="container principalFinalReview" style={{"padding":"1px"}} onSubmit={save} >               
+           <div className="container principalFinalReview" style={{"padding":"1px"}}  >               
                 <div className="row titulo" style={{textAlign: "left",marginTop:"25px",}}>
-                    <h1>Revisión de Convenio</h1>
+                    <h1>Revisión de Convenio</h1>                    
                 </div>
                 <div className="shadowbox">
                     <div className="row normalrow" style={{textAlign: "justify", marginTop:"10px"}}>
@@ -113,31 +221,28 @@ export default function AgreementReview (){
                             Aquí podrá revisar el convenio y plan de aprendizaje del alumno(a). 
                             Podrá descargar los archivos enviados dando click en los mismos o podrá verlos presionando el boton
                             a la derecha de los mismos. Luego de ello, podrá dar decidir si aprueba los documentos o en todo caso 
-                            ingresar una observación de los mismos. Para finalizar la revisión, presione el boton Guardar.   
-                        </p>
+                            ingresar una observación de los mismos. Para finalizar la revisión, presione el boton Guardar. <br></br><br></br>
+                            A continuación podrá descargar las rúbricas de ambos documentos. 
+                        </p>                        
                     </div> 
+                    <ShowFiles docs={docs} />
                 </div>
                 <div className="shadowbox">
                     <div className="row row1" style={{textAlign: "left",marginTop:"25px"}}>
                         <h2 className="subtitulo">Estado de la revisión</h2>
                     </div>
                     <div className="row normalrow" style={{marginTop:"10px"}}>
-                        <StateViewer states={[StatesViewType[typeApprovalStateFACI]("Aprobación FACI", data.faciState),
-                        StatesViewType[typeApprovalStateEsp]("Aprobación Especialidad", data.espState)]}/>
+                        <StateViewer states={[StatesViewType[typeApprovalStateFACI]("Aprobación FACI",comentarioFACI),
+                        StatesViewType[typeApprovalStateEsp]("Aprobación Especialidad", comentarioEsp)]}/>
                     </div>
                 </div>
                 <div className="shadowbox">
                     <div className="row row1" style={{textAlign: "left",marginTop:"25px"}}>
-                        <h2 className="subtitulo">Documentos enviados</h2>                    
-                    </div>
-                    <div className="row normalrow" style={{marginLeft:"10px"}}>
-                        <DocumentPlusIcon name="Convenio Oscar Navarro.pdf" url={Convenio}/>
-                        <Button variant="primary" size="sm" style={{width: "100px",marginTop:"5px", marginLeft:"101px"}}>Visualizar</Button>{' '}
-                    </div>
-                    <div className="row normalrow" style={{marginLeft:"10px",marginTop:"10px"}}>
-                        <DocumentPlusIcon name="Plan de Aprendizaje Oscar Navarro.pdf" url={Convenio}/>
-                        <Button variant="primary" size="sm" style={{width: "100px", marginTop:"5px",marginLeft:"30px"}}>Visualizar</Button>{' '}
-                    </div>
+                        <h2 className="subtitulo">Documentos enviados por el alumno</h2>                    
+                    </div>  
+                    <ShowFiles docs={docsStudent} />
+                    {/* <FileManagement canUpload={false} docs={docsStudent} maxFiles={2} titulo="Documentos a enviar al alumno"/>                  */}
+                                       
                 </div>
                 <div className="shadowbox">
                     <div className="row row1" style={{textAlign: "left",marginTop:"30px"}}>
@@ -179,30 +284,29 @@ export default function AgreementReview (){
                         </Form>
                     </div>
                 </div>
+                
+                <div className="shadowbox">
+                    <div className="row row2" style={{textAlign: "left",marginTop:"25px"}}>
+                        <h2 className="subtitulo">Observaciones</h2>                                               
+                            <Form.Control className="observaciones"
+                                placeholder="Escriba las observaciones de la entrega"                                                    
+                                onChange = {changeComments}
+                                defaultValue = {data.entregaConvenioyPlan.observations} 
+                                name="comments"
+                                as="textarea" 
+                                rows={10}                                                             
+                            />                                         
+                    </div>
+                </div>
                 <div className="shadowbox">
                     <div className="row row1" style={{textAlign: "left",marginTop:"25px"}}>                                       
-                        <FileManagement name="Documentos a enviar al alumno"/>                        
+                        <FileManagement canUpload={true} docs={docsCoord} maxFiles={2} titleUpload="Documentos a enviar al alumno" titleUploadedFiles="Documentos enviados al alumno"/>
                     </div>
                 </div>
-                <div className="shadowbox">
-                    <div className="row row1" style={{textAlign: "left",marginTop:"25px"}}>
-                        <h2 className="subtitulo">Observaciones</h2>  
-                        <Form>                        
-                            <Form.Group className="mb-3" controlId="ControlTextarea1">                            
-                                <Form.Control 
-                                    as="textarea" 
-                                    rows={10} 
-                                    value = {data.reviewCoord.observations}
-                                    onChange = {changeComments}                                    
-                                />
-                            </Form.Group>
-                        </Form>                           
-                    </div>
-                </div>
-                <div className="row botones" style={{marginLeft:"10px"}}>                    
+                {/* <div className="row botones" style={{marginLeft:"10px"}}>                    
                     <Button  className="btn btn-sec" style={{width:"20%",marginRight:"50px"}}>Regresar</Button>                   
-                    <Button  className="btn btn-pri" style={{width:"20%",marginLeft:"50px"}}>Guardar</Button>                  
-                </div>           
+                    <Button  className="btn btn-pri" style={{width:"20%",marginLeft:"50px"}} onClick={update}>Guardar</Button>                  
+                </div>  */}          
             </div>   
         </LayoutCoordFACI>
     );
