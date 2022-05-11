173bfe2173ed87946c7e5f3d56c655c3fa808c52
Oscar Daniel <oscar.navarro@pucp.edu.pe>
2022-05-11 14:50:57 -0500

    Back schedule

--- client/src/pages/SupervisorSelection.js
+++ client/src/pages/SupervisorSelection.js
@@ -90,7 +90,7 @@ export default function SupervisorSelection () {
                         <SupervisorSelector supervisores={supervisores} setSupervisores={setSupervisores}/>
                 </div>
                 <div className="row rows">
-                    <Timetable/>
+                    <Timetable idSupervisor={9}/>
                 </div>
                 <div className="row rows boton">
                     <Button className="btn btn-primary" style={{width:"40%"}}>Agendar</Button>
