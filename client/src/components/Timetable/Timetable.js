df529da3b5f4d0fd075d4c9df5d66621d5bcbb00
Oscar Daniel <oscar.navarro@pucp.edu.pe>
2022-05-11 08:03:02 -0500

    correccion del comp horario

--- client/src/components/Timetable/Timetable.js
+++ client/src/components/Timetable/Timetable.js
@@ -1,247 +1,74 @@
-import React, { useState }  from "react";
+import React, { useState }  from "react"; 
 
 import './Timetable.scss';
 import TimetableCell from "./TimetableCell/Timetablecell";
 
+const hourRange = [
+    "08:00",
+    "09:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00"
+]
+
+const dataDummy = [
+    {
+        date: "04-05-2022",
+        hours: [1,1,1,3,2,2,4,4,4,2,2,2,1,1]
+    },
+    {
+        date: "05-05-2022",
+        hours: [1,1,1,2,2,2,4,4,4,2,2,2,1,1]
+    },
+    {
+        date: "06-05-2022",
+        hours: [1,1,1,2,2,2,4,4,4,2,2,2,1,1]
+    },
+    {
+        date: "07-05-2022",
+        hours: [1,1,1,2,2,2,4,4,4,2,2,2,1,1]
+    },
+    {
+        date: "08-05-2022",
+        hours: [1,1,1,2,2,2,4,4,4,2,2,2,1,1]
+    },
+    {
+        date: "09-05-2022",
+        hours: [1,1,1,2,2,2,4,4,4,2,2,2,1,1]
+    },
+    {
+        date: "10-05-2022",
+        hours: [1,1,1,2,2,2,4,4,4,2,2,2,1,1]
+    },
+    {
+        date: "11-05-2022",
+        hours: [1,1,1,2,2,2,4,4,4,2,2,2,1,1]
+    },
+    {
+        date: "12-05-2022",
+        hours: [1,1,1,2,2,2,4,4,4,2,2,2,1,1]
+    },
+    {
+        date: "13-05-2022",
+        hours: [1,1,1,2,2,2,4,4,4,2,2,2,1,1]
+    },
+    {
+        date: "14-05-2022",
+        hours: [1,1,1,2,2,2,4,4,4,2,2,2,1,1]
+    },
+]
 
 export default function Timetable ({horario, states}){
-    const [inputs, setInputs] = useState({
-        days: [
-            {
-                name: "Lunes 4",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 3 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 4 },
-                    { state: 4 },
-                    { state: 4 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 }
-                ]
-            }, {
-                name: "Martes 5",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 }
-                ]
-            }, {
-                name: "Miercoles 6",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 }
-                ]
-            }, {
-                name: "Jueves 7",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 }
-                ]
-            }, {
-                name: "Viernes 8",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 }
-                ]
-            }, {
-                name: "Sabado 7",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 }
-                ]
-            },{
-                name: "Lunes 9",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 }
-                ]
-            }, {
-                name: "Martes 10",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 }
-                ]
-            }, {
-                name: "Miercoles 11",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 }
-                ]
-            }, {
-                name: "Jueves 12",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 }
-                ]
-            }, {
-                name: "Viernes 13",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 }
-                ]
-            }, {
-                name: "Sabado 14",
-                hours: [
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 1 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 2 },
-                    { state: 1 },
-                    { state: 1 }
-                ]
-            }
-        ],
-        hourRange: [
-            "08:00",
-            "09:00",
-            "10:00",
-            "11:00",
-            "12:00",
-            "13:00",
-            "14:00",
-            "15:00",
-            "16:00",
-            "17:00",
-            "18:00",
-            "19:00",
-            "20:00",
-            "21:00"
-        ]
-    })
+    const [inputs, setInputs] = useState(dataDummy)
     const [indexs, setIndex] = useState({
         actual:0, // page actual
         separator: 6
@@ -256,19 +83,19 @@ export default function Timetable ({horario, states}){
     }
     const rigthPage = e => {
         // Verifica que no se salga del limite 
-        if (inputs , Math.ceil(inputs.days.length/indexs.separator) -1 > indexs.actual){
+        if (inputs , Math.ceil(inputs.length/indexs.separator) -1 > indexs.actual){
             setIndex({...indexs,
                 ['actual']: (indexs.actual + 1)})
         }
     }
-    var diaInicio = ""
-    var diaFin = ""
-    if (inputs, inputs.days) {
-        diaInicio = inputs.days[indexs.actual*indexs.separator].name
-        diaFin = inputs.days[(indexs.actual+1)*indexs.separator-1].name
+    let diaInicio = ""
+    let diaFin = ""
+    if (inputs) {
+        diaInicio = inputs[indexs.actual*indexs.separator].date
+        diaFin = inputs[(indexs.actual+1)*indexs.separator-1].date
     }
     
-    return ( inputs && inputs.days &&
+    return ( inputs &&
         <div>
             <div className="indicatorPage">
                     <div className="btn btn-primary left" onClick={leftPage}>Anterior</div>
@@ -279,19 +106,19 @@ export default function Timetable ({horario, states}){
                 <div className="col col-2 col-lg-1">
                     <div className="headerTime">Hora</div>
                      {
-                        inputs.hourRange && inputs.hourRange.map((hour, index) => (
-                        <p className="hourRangeCell">{hour}</p>
+                        hourRange && hourRange.map((hour, index) => (
+                            <p className="hourRangeCell">{hour}</p>
                         ))
                     }
                 </div>
                 {
-                    inputs.days.map( (day, indexDay) => {
+                    inputs.map( (day, indexDay) => {
                         return (
                             (indexDay < (indexs.actual + 1)*indexs.separator && indexDay >= indexs.actual * indexs.separator) && <div className="col">
-                            <div className="headerTime">{day.name}</div>
+                            <div className="headerTime">{day.date}</div>
                             {
-                            day.hours.map((hour, indexHour) => (
-                                <TimetableCell hour={hour} change={setInputs} inputsSuper={inputs} indexDay={indexDay} indexHour ={indexHour}></TimetableCell>
+                            day.hours.map((state, indexHour) => (
+                                <TimetableCell state={state} setInputs={setInputs} inputs={inputs} indexDay={indexDay} key={indexHour} indexHour={indexHour}></TimetableCell>
                             ))
                             }
                         </div>
