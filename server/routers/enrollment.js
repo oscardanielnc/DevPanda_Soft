35aa8d14913c1f3ede92a42a07da6a8b907ba43c
Oscar Daniel <oscar.navarro@pucp.edu.pe>
2022-05-29 22:45:58 -0500

    excel redi

--- server/routers/enrollment.js
+++ server/routers/enrollment.js
@@ -4,5 +4,6 @@ const EnrollmentController = require("../controllers/enrollment");
 const api = express.Router();
 
 api.get("/enrollment-students/:fidProceso", EnrollmentController.selectStudentsByProcessSpecialty); 
+api.put("/enrollment-students-ids", EnrollmentController.updateEnrollmentStudents); 
 
 module.exports = api;
