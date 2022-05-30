d2eca07db6ea0d136f3f079adbddf488b961e0c1
Oscar Daniel <oscar.navarro@pucp.edu.pe>
2022-05-30 10:33:44 -0500

    otra correccion de rutas 2

--- client/src/pages/General/RedirectPage.js
+++ client/src/pages/General/RedirectPage.js
@@ -6,12 +6,12 @@ export default function AddSpecialty () {
         window.location.href = `/`
     } else {
         if(user.tipoPersona==="e")
-            window.location.href = `/welcome-process/idStudent=${user.idPersona}&idProcess=${user.fidProceso}&phase=WPRO`;
+            window.location.href = `/welcome-process/idStudent=${user.idPersona}&phase=WPRO`;
         else {
             switch (user.tipoPersonal) {
                 case 'S': 
                     window.location.href = `/meetings-management/permissions=S&idSupervisor=${user.idPersona}&idProcess=${user.fidProceso}`; break;
-                case 'E': window.location.href = `/students-management/permissions=E&idProcess=${user.fidProceso}`; break;
+                case 'E': window.location.href = `/students-management/permissions=E`; break;
                 case 'F': window.location.href = `/specialty-management/permissions=AF`; break;
                 case 'A': window.location.href = `/specialty-management/permissions=AF`; break;
                 default: window.location.href = `/list-supervisors/permissions=C`; break;
