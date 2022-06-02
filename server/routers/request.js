38e1601a12463ce007bd8c2f1828f8046a4e0abc
Carlo Ames <carlo.ames@pucp.edu.pe>
2022-06-02 00:43:15 -0500

    cambios

--- server/routers/request.js
+++ server/routers/request.js
@@ -4,7 +4,6 @@ const RequestController = require("../controllers/request");
 const api = express.Router();
 
 api.get("/requestList/:idEspecialidad",RequestController.requestList);
-api.get("/requestListAgreement/:idEspecialidad",RequestController.requestListAgreement);
 
 api.get("/verifyRequest/:fidAlumno",RequestController.verifyRequest);
 
