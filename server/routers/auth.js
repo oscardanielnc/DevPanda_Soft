687c62dd3b57c42882262763f6556481010dc8b6
Oscar Daniel <oscar.navarro@pucp.edu.pe>
2022-06-08 14:42:08 -0500

    foto corregida

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -3,7 +3,7 @@ const AuthController = require("../controllers/auth");
 
 const api = express.Router();
 
-api.get("/sign-in/:email", AuthController.singIn);
+api.post("/sign-in", AuthController.singIn);
 api.post("/sign-up", AuthController.signUp);
 
 module.exports = api;
